2c327357150faeec5c775d2d903a23cebf197d4b
kobabasu <keiji.kobayashi.web@gmail.com>
2015-06-07 21:41:32 +0900

    first commit

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+use Phalcon\Config;
+
+return new Config(array(
+  'baseurl' => '/api/',
+
+  'app' => array(
+    'modelsDir'      => 'models/'
+  )
+));
---
+++ models/Users.php
@@ -0,0 +1,6 @@
+<?php
+use Phalcon\Mvc\Model;
+
+class Users extends Model
+{
+}
